d61cf55eff88d96e36dc0e43ba822ffe884def5d
Marcelo Colomer <mcolomercornejo@confluent.io>
2022-06-09 09:31:27 +0200

    upgrade version

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     confluent = {
       source  = "confluentinc/confluent"
-      version = "0.9.0"
+      version = "0.10.0"
     }
   }
   experiments = [module_variable_optional_attrs]
